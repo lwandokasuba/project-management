475b7cf72f2301e453c961f204ab51544ca7018e
lwandokasuba <lwandojapkas@gmail.com>
2025-04-17 18:11:53 +0200

    Add BaseEntity and Project models with status enumeration

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Projects.Models
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,20 @@
+namespace Projects.Models
+{
+  public class Project : BaseEntity
+  {
+    public int ProjectId { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public required DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public required ProjectStatus Status { get; set; } = ProjectStatus.CREATED;
+  }
+
+  public enum ProjectStatus
+  {
+    CREATED,
+    IN_PROGRESS,
+    COMPLETED,
+    SUSPENDED
+  }
+}
