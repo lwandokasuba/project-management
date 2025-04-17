be0ae5622eabad70dde8a999ea768ce63696bbc8
lwandokasuba <lwandojapkas@gmail.com>
2025-04-17 18:12:03 +0200

    Add Projects DbSet and configure Project entity in ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,17 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Projects.Models;
 
 namespace Projects.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+  public DbSet<Project> Projects { get; set; } = null!;
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+    builder.Entity<Project>().ToTable("Projects")
+      .HasKey(p => p.ProjectId);
+  }
 }
