846247c49179030d32f6aad7e1eebabea3b20abc
lwandokasuba <lwandojapkas@gmail.com>
2025-04-17 18:20:21 +0200

    Remove Radzen components from App and MainLayout, and update Program.cs to streamline component usage

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddRadzenComponents();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
