391dece6a50afe7b3b522a5d2a7034b7c972903f
lwandokasuba <lwandojapkas@gmail.com>
2025-06-12 12:44:37 +0200

    Implement database connection and schema creation in main.go

--- go-gin/main.go
+++ go-gin/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/lwandokasuba/project-management/ent"
+	_ "github.com/lib/pq"
 )
 
 var router *gin.Engine
@@ -32,6 +37,23 @@ func main() {
 
 	router.Run()
 }
+
+func connectDB(cfg config) *ent.Client {
+	client, err := ent.Open("postgres", "host="+cfg.DATABASE_HOST+" user="+cfg.DATABASE_USER+
+		" password="+cfg.DATABASE_PASSWORD+" dbname="+cfg.DATABASE_NAME+
+		" port="+cfg.DATABASE_PORT+" sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	// defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	return client
+}
+
 type config struct {
 	DATABASE_USER     string `env:"DATABASE_USER" envDefault:"postgres"`
 	DATABASE_PASSWORD string `env:"DATABASE_PASSWORD" envDefault:"password"`
