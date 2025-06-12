79d54e97be0e2331c619a9a68c7f6546ac1340ea
lwandokasuba <lwandojapkas@gmail.com>
2025-06-12 12:40:55 +0200

    Load environment variables and define configuration struct

--- go-gin/main.go
+++ go-gin/main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var router *gin.Engine
 
 func main() {
+	cfg := loadEnv()
 
 	router = gin.Default()
 
@@ -28,4 +31,26 @@ func main() {
 	})
 
 	router.Run()
+}
+type config struct {
+	DATABASE_USER     string `env:"DATABASE_USER" envDefault:"postgres"`
+	DATABASE_PASSWORD string `env:"DATABASE_PASSWORD" envDefault:"password"`
+	DATABASE_HOST     string `env:"DATABASE_HOST" envDefault:"localhost"`
+	DATABASE_PORT     string `env:"DATABASE_PORT" envDefault:"5432"`
+	DATABASE_NAME     string `env:"DATABASE_NAME" envDefault:"database"`
+}
+
+func loadEnv() config {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	var cfg config
+	cfg, err = env.ParseAs[config]()
+	if err != nil {
+		panic("failed to get environment variables")
+	}
+
+	return cfg
 }
