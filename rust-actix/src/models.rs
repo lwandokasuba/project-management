ceb42df376b4595385fdb95c8a1ec31569cd5913
lwandokasuba <lwandojapkas@gmail.com>
2025-06-17 18:52:08 +0200

    Add id to user struct

--- rust-actix/src/models.rs
+++ rust-actix/src/models.rs
@@ -4,6 +4,7 @@ use tokio_pg_mapper_derive::PostgresMapper;
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "users")] // singular 'user' is a keyword..
 pub struct User {
+    pub id: i64,
     pub email: String,
     pub first_name: String,
     pub last_name: String,
